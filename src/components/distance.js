e1663b23473cfa563e12c146e0e67d043ae3364e
Christopher Karras <ckarras11@gmail.com>
2018-03-15 20:32:16 -0400

    Added normalize string function for locations

--- src/components/distance.js
+++ src/components/distance.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+const normalizeStr = (s) => {
+    (s.charAt(0).concat(s.substr(1).toLowerCase())
+}
+
 const Distance = (props) => {
     return (
         <div>
-            <h2>{`There are ${props.miles} miles between ${props.loc1} and ${props.loc2}`}</h2>
+            <h2>{`There are ${props.miles} miles between ${normalizeStr(props.loc1)} and ${normalizeStr(props.loc2)}`}</h2>
         </div>
     );
 };
