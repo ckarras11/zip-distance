73b43a8e72502afa824b4b8ce16d5a8cafb37aef
Christopher Karras <ckarras11@gmail.com>
2018-03-15 21:00:42 -0400

    Changed props to destructuring

--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Form = (props) => {
+const Form = ({onSubmit, handleZip1, handleZip2}) => {
     return (
         <div>
-            <form className="form-horizontal" onSubmit={props.onSubmit}>
+            <form className="form-horizontal" onSubmit={onSubmit}>
                 <label htmlFor="zip1">Zip Code 1</label>
-                <input className="form-control" id="zip1" name="zip1" type="number" onChange={props.handleZip1}/>
+                <input className="form-control" id="zip1" name="zip1" type="number" onChange={handleZip1}/>
                 <label htmlFor="zip2">Zip Code 2</label>
-                <input className="form-control" id="zip2" name="zip2" type="number" onChange={props.handleZip2}/>
+                <input className="form-control" id="zip2" name="zip2" type="number" onChange={handleZip2}/>
                 <input className="btn" type="submit" />
             </form>
         </div>
