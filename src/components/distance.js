47d2eb57698ba64daba29974784021434fa7df54
Christopher Karras <ckarras11@gmail.com>
2018-03-15 20:58:40 -0400

    Normalized location names

--- src/components/distance.js
+++ src/components/distance.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const normalizeStr = (s) => {
-    (s.charAt(0).concat(s.substr(1).toLowerCase())
-}
+const norm = (s) => s.toLowerCase().replace(/\b[a-z]/g, (f) => f.toUpperCase())
 
-const Distance = (props) => {
+const Distance = ({miles, loc1, loc2}) => {
     return (
         <div>
-            <h2>{`There are ${props.miles} miles between ${normalizeStr(props.loc1)} and ${normalizeStr(props.loc2)}`}</h2>
+            <h2>{`There are ${miles} miles between ${norm(loc1)} and ${norm(loc2)}`}</h2>
         </div>
     );
 };
